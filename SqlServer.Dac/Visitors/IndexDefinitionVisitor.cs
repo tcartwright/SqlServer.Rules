41250ac5b41ae2c8eeda700ee12f99e6d1b78bb8
Matt N <matthew.naul@gmail.com>
2020-11-21 15:34:54 -0800

    feat(file): Missing a Index visitor

---
+++ SqlServer.Dac/Visitors/IndexDefinitionVisitor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System.Collections.Generic;
+
+namespace SqlServer.Dac.Visitors
+{
+    public class IndexDefinitionVisitor : BaseVisitor, IVisitor<IndexDefinition>
+    {
+        public IList<IndexDefinition> Statements { get; } = new List<IndexDefinition>();
+		public int Count { get { return this.Statements.Count; } }
+		public override void ExplicitVisit(IndexDefinition node)
+        {
+            Statements.Add(node);
+        }
+    }
+}
