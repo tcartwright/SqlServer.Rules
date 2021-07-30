e64f4eb5b91b1b5d14410b1d72c97c72b86ee9d1
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-07-30 16:25:28 -0500

    - added util method to dump scriptoken streams as string - rearranged test cases folders slightly - added global ignore and extra query to nonsargable_ingored.sql

--- SqlServer.Rules.Test/TestScripts/Performance/AvoidNonsargableLikePattern/nonsargable_ignored.sql
+++ SqlServer.Rules.Test/TestScripts/Performance/AvoidNonsargableLikePattern/nonsargable_ignored.sql
@@ -1,5 +1,7 @@
 ﻿CREATE VIEW [dbo].[nonsargable_view_ignored] AS
-	-- IGNORE SRP0002
+	-- GLOBAL IGNORE SRP0002
 	SELECT table1_id FROM [dbo].[table1] WHERE [data] LIKE '%what am I doing?'
+	UNION ALL
+	SELECT table1_id FROM [dbo].[table1] WHERE [data] LIKE '%I love lamp?'
 
 GO
