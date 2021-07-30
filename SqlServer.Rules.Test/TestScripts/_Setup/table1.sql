b5c53fb88c44b9f7926a51a911236617b2ddc00c
Puckett <delgertome@gmail.com>
2021-07-30 13:44:33 -0500

    test: Initial test for nonSARGable like patterns

---
+++ SqlServer.Rules.Test/TestScripts/_Setup/table1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[table1] (
+	[table1_id] INT IDENTITY (1000, 1) NOT NULL,
+	[data] VARCHAR(1000) NULL,
+	[modified_date] DATETIME2(7) NOT NULL,
+
+	CONSTRAINT [pk_table1] PRIMARY KEY CLUSTERED (table1_id)
+)
+
+GO
