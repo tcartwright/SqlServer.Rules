76dacf93ec248cebcab08947c41437e862ccaa7d
Puckett <delgertome@gmail.com>
2021-07-30 22:13:55 -0500

    refactor: moved fetching SQL rules problems to base class for clearner tests

--- SqlServer.Rules.Test/TestCasesBase.cs
+++ SqlServer.Rules.Test/TestCasesBase.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.SqlServer.Dac.Model;
+﻿using Microsoft.SqlServer.Dac.CodeAnalysis;
+using Microsoft.SqlServer.Dac.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlServer.Rules.Test;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace SqlServer.Rules.Tests
 {
@@ -9,5 +14,26 @@ namespace SqlServer.Rules.Tests
     {
         protected const SqlServerVersion SqlVersion = SqlServerVersion.Sql130;
         protected StringComparer Comparer = StringComparer.OrdinalIgnoreCase;
+
+        public TestContext TestContext { get; set; }
+
+        protected ReadOnlyCollection<SqlRuleProblem> GetTestCaseProblems(string testCases, string ruleId)
+        {
+            ReadOnlyCollection<SqlRuleProblem> problems = new ReadOnlyCollection<SqlRuleProblem>(new List<SqlRuleProblem>());
+
+            using (var test = new BaselineSetup(TestContext, testCases, new TSqlModelOptions(), SqlVersion))
+            {
+                try
+                {
+                    test.RunTest(ruleId, (result, problemString) => problems = result.Problems);
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail($"Exception thrown for ruleId '{ruleId}' for test cases '{testCases}': {ex.Message}");
+                }
+            }
+
+            return problems;
+        }
     }
 }
