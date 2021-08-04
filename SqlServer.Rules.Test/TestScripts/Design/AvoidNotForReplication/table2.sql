5c5bf2693899d9dccc2c74ec56d0f895c650f197
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-08-03 20:30:48 -0500

    Added new AvoidNotForReplication rule and unit tests

---
+++ SqlServer.Rules.Test/TestScripts/Design/AvoidNotForReplication/table2.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[table2] (
+	[table2_id] INT IDENTITY (1000, 1) NOT NULL,
+	[table1_id] INT NOT NULL,
+	[data] VARCHAR(1000) NULL,
+	[modified_date] DATETIME2(7) NOT NULL,
+	CONSTRAINT [pk_table2] PRIMARY KEY CLUSTERED (table2_id),
+	CONSTRAINT [CK_table2] CHECK (data IS NOT NULL)
+)
+
+GO
---
+++ SqlServer.Rules/Design/AvoidNotForReplication.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.SqlServer.Dac.CodeAnalysis;
+using Microsoft.SqlServer.Dac.Model;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+using SqlServer.Dac.Visitors;
+using SqlServer.Rules.Globals;
+using SqlServer.Rules.ReferentialIntegrity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SqlServer.Rules.Design
+{
+    [ExportCodeAnalysisRule(RuleId,
+        RuleDisplayName,
+        Description = RuleDisplayName,
+        Category = Constants.Design,
+        RuleScope = SqlRuleScope.Element)]
+    public sealed class AvoidNotForReplication : BaseSqlCodeAnalysisRule
+    {
+        public const string RuleId = Constants.RuleNameSpace + "SRD0065";
+        public const string RuleDisplayName = Message;
+        public const string Message = "Avoid 'NOT FOR REPLICATION' unless this is the desired behavior and replication is in use.";
+
+        public AvoidNotForReplication() : base(
+            ModelSchema.ForeignKeyConstraint,
+            ModelSchema.CheckConstraint,
+            ModelSchema.DmlTrigger,
+            ModelSchema.Table)
+        {
+        }
+
+        public override IList<SqlRuleProblem> Analyze(SqlRuleExecutionContext ruleExecutionContext)
+        {
+            var problems = new List<SqlRuleProblem>();
+            var sqlObj = ruleExecutionContext.ModelElement;
+
+            if (sqlObj == null || sqlObj.IsWhiteListed())
+                return problems;
+
+            var obj = ruleExecutionContext.SchemaModel.GetObject(sqlObj.ObjectType, sqlObj.Name, DacQueryScopes.All);
+
+            bool notForReplication = false;
+
+            if (sqlObj.ObjectType == ForeignKeyConstraint.TypeClass)
+            {
+                notForReplication = Convert.ToBoolean(sqlObj.GetProperty(ForeignKeyConstraint.NotForReplication));
+            }
+            else if (sqlObj.ObjectType == CheckConstraint.TypeClass)
+            {
+                notForReplication = Convert.ToBoolean(sqlObj.GetProperty(CheckConstraint.NotForReplication));
+            }
+            else if (sqlObj.ObjectType == DmlTrigger.TypeClass)
+            {
+                notForReplication = Convert.ToBoolean(sqlObj.GetProperty(DmlTrigger.NotForReplication));
+            }
+            else if (sqlObj.ObjectType == Table.TypeClass)
+            {
+                var createTable = ruleExecutionContext.ScriptFragment as CreateTableStatement;
+                var identityColumn = createTable.Definition.ColumnDefinitions.FirstOrDefault(cd => cd.IdentityOptions != null);
+                notForReplication = identityColumn.IdentityOptions.IsIdentityNotForReplication;
+            }
+
+            if (notForReplication)
+            {
+                problems.Add(new SqlRuleProblem(Message, sqlObj));
+            }
+
+            return problems;
+        }
+    }
+}
