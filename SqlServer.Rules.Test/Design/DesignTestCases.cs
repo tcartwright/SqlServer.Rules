cd956840256835630727da0b3982a1cd6b614358
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-08-03 20:30:48 -0500

    Added new AvoidNotForReplication rule and unit tests

---
+++ SqlServer.Rules.Test/Design/DesignTestCases.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlServer.Rules.Test;
+using System;
+using System.Linq;
+using Microsoft.SqlServer.Dac.Model;
+using SqlServer.Rules.Performance;
+using Microsoft.SqlServer.Dac.CodeAnalysis;
+using System.Text;
+using System.Collections.Generic;
+using SqlServer.Rules.Design;
+
+namespace SqlServer.Rules.Tests.Performance
+{
+    [TestClass]
+    [TestCategory("Performance")]
+    public class DesignTestCases : TestCasesBase
+    {
+        [TestMethod]
+        public void TestAvoidNotForReplication()
+        {
+            var problems = GetTestCaseProblems("AvoidNotForReplication", AvoidNotForReplication.RuleId);
+
+            var expected = 4;
+            Assert.AreEqual(expected, problems.Count, $"Expected {expected} problem(s) to be found");
+
+            Assert.IsTrue(problems.Any(problem => Comparer.Equals(problem.SourceName, "dbo_table2_trigger_1_not_for_replication.sql")));
+            Assert.IsTrue(problems.Any(problem => Comparer.Equals(problem.SourceName, "fk_table2_table1_1_not_for_replication.sql")));
+            Assert.IsTrue(problems.Count(problem => Comparer.Equals(problem.SourceName, "table3.sql")) == 2);
+
+            Assert.IsTrue(problems.All(problem => Comparer.Equals(problem.Description, AvoidNotForReplication.Message)));
+            Assert.IsTrue(problems.All(problem => problem.Severity == SqlRuleProblemSeverity.Warning));
+        }
+    }
+}
---
+++ SqlServer.Rules.Test/TestScripts/Design/AvoidNotForReplication/dbo_table2_trigger_2.sql
@@ -0,0 +1,3 @@
+﻿CREATE TRIGGER [dbo_table2_trigger_2] ON dbo.table2 AFTER INSERT AS
+	SELECT * FROM Inserted;
+GO
