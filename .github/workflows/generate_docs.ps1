b4efd2ea99ae468228927565be7b17755bf5146c
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-08-06 20:36:31 -0500

    - added debugging statements to output

--- .github/workflows/generate_docs.ps1
+++ .github/workflows/generate_docs.ps1
@@ -16,6 +16,8 @@ if (!(Test-Path -Path $docs -PathType Container)) {
 #Write dummy file to docs folder
 "#Hello World! The time is now: $(Get-Date -Format "h:mm:ss tt")" | Out-File -FilePath "$($docs)README.MD" -Force -Encoding ascii 
 
+Get-Content -Path "$($docs)README.MD" -Raw
+
 
 Add-Type -Path "$path\SqlServer.Rules\bin\Debug\net472\SqlServer.Rules.dll" -IgnoreWarnings -ErrorAction SilentlyContinue 
 
