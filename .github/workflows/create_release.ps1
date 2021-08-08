f37cff258cffc592fb26430a55e2e6b0a6ba7f5a
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-08-08 12:14:16 -0500

    Create Release
    - added new simple install ps so the releases can be installed
    - added new create release ps script to create a release zip
    - modifed the pipeline to use https://github.com/marketplace/actions/github-tag-bump

---
+++ .github/workflows/create_release.ps1
@@ -0,0 +1,39 @@
+﻿Param(
+    [Parameter(Mandatory=$true)]
+    [string] $ParentDirectory,
+    [Parameter(Mandatory=$true)]
+    [string] $BuildDirectory,
+    [string] $version = "1.0.0"
+)
+
+Clear-Host
+
+# ensure we dont have trailing back slashes, so we dont double up
+$ParentDirectory = $ParentDirectory.TrimEnd("\\")
+$BuildDirectory = $BuildDirectory.TrimEnd("\\")
+
+$temp = $env:TEMP
+$rulesDir = "$temp\SqlServer.Rules"
+$releaseDir = [System.IO.Path]::Combine($temp, "release")
+$docsDir = [System.IO.Path]::Combine($ParentDirectory, "docs")
+
+if (!(Test-Path $rulesDir)) {
+    New-Item $rulesDir -ItemType Directory | Out-Null
+}
+
+if (!(Test-Path $releaseDir)) {
+    New-Item $releaseDir -ItemType Directory | Out-Null
+}
+
+Copy-Item -path "$BuildDirectory\*.*" -Destination $rulesDir
+
+$compress = @{
+  Path = $docsDir, $rulesDir, "$ParentDirectory\README.md"
+  CompressionLevel = "Fastest"
+  DestinationPath = "$releaseDir\release.$version.zip"
+}
+
+Compress-Archive @compress -Force -Verbose
+
+Write-Host "Release written to: " -NoNewline
+Write-Host "$releaseDir\release.$version.zip" -ForegroundColor Yellow
