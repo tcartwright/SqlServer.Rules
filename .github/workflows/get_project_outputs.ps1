d61788bd84894b9e579e84be5e192cebc26799a9
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-08-07 20:54:18 -0500

    Powershell to extract build info
    Wrote powershell to extract the build path, and dll path from the project and place into environment variables
    will be using for the generate docs, and release creation

---
+++ .github/workflows/get_project_outputs.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+    [Parameter(Mandatory=$true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [string] $ProjectPath, 
+    [string] $Configuration = "Release",
+    [string] $Platform = "AnyCPU",
+    [string] $TargetFramework = "^net.*"
+)
+Clear-Host
+
+[xml]$csproj = (Get-Content -Path $ProjectPath -Raw) 
+
+$framework = $csproj.Project.PropertyGroup.TargetFramework -split ";|," | Where-Object { $_ -match $TargetFramework } | Select-Object -First 1
+
+if ([string]::IsNullOrWhiteSpace($framework)) {
+    throw "Invalid framework"
+    exit 1
+}
+
+$releaseConfig = $csproj.Project.PropertyGroup | Where-Object { $_.Condition -ieq  " '`$(Configuration)|`$(Platform)' == '$Configuration|$Platform' " }
+$assemblyName = $csproj.Project.PropertyGroup.AssemblyName | Select-Object -First 1
+
+if ([string]::IsNullOrWhiteSpace($assemblyName)) {
+    # if not set, assume the project name
+    $assemblyName = [System.IO.Path]::GetFileNameWithoutExtension($ProjectPath) 
+}
+
+$outputPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($ProjectPath), $releaseConfig.OutputPath, $framework)
+$outputDll = [System.IO.Path]::Combine($outputPath, "$assemblyName.dll")
+
+Write-Output "::set-env name=BUILD_OUTPUT_PATH::'$outputPath'"
+Write-Output "::set-env name=BUILD_OUTPUT_DLL::'$outputDll'"
