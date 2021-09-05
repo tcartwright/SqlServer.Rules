4ea0a611ed3db4f0eadd5399ad597fc44cc8ad6e
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-09-05 10:30:17 -0500

    - updating release name to be repo name

--- .github/workflows/create_release.ps1
+++ .github/workflows/create_release.ps1
@@ -1,9 +1,13 @@
-﻿Param(
+﻿﻿Param(
     [Parameter(Mandatory=$true)]
-    [string] $ParentDirectory,
+    [string] 
+    $ParentDirectory,
     [Parameter(Mandatory=$true)]
-    [string] $BuildDirectory,
-    [string] $version = "1.0.0"
+    [string] 
+    $BuildDirectory,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $releasePath
 )
 
 Clear-Host
@@ -14,7 +18,7 @@ $BuildDirectory = $BuildDirectory.TrimEnd("\\")
 
 $temp = $env:TEMP
 $rulesDir = "$temp\SqlServer.Rules"
-$releaseDir = [System.IO.Path]::Combine($temp, "release")
+
 $docsDir = [System.IO.Path]::Combine($ParentDirectory, "docs")
 
 if (!(Test-Path $rulesDir)) {
@@ -22,23 +26,15 @@ if (!(Test-Path $rulesDir)) {
     New-Item $rulesDir -ItemType Directory | Out-Null
 }
 
-if (!(Test-Path $releaseDir)) {
-    Write-Host "Creating $releaseDir"
-    New-Item $releaseDir -ItemType Directory | Out-Null
-}
 
 Copy-Item -path "$BuildDirectory\*.*" -Destination $rulesDir
 
-$releaseName = "release.zip" # removing version until I can figure out how to get the version tags from master
 $compress = @{
   Path = $docsDir, $rulesDir, "$ParentDirectory\README.md"
   CompressionLevel = "Fastest"
-  DestinationPath = "$releaseDir\$releaseName"
+  DestinationPath = $releasePath
 }
 
 Compress-Archive @compress -Force -Verbose 
 
-Write-Host "Release written to: " -NoNewline
-Write-Host "$releaseDir\$releaseName" -ForegroundColor Yellow
-
-echo "GITHUB_RELEASE_PATH=$releaseDir\$releaseName" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+Write-Host "Release written to: $releasePath" 
