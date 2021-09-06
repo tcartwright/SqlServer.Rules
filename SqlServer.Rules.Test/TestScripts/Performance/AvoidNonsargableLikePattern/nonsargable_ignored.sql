ecbd87419478cd15e80137db137d7a5d32a501a3
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-09-06 11:29:18 -0500

    - CREDITS: Matthew Naul - he created these test cases but because of when he branched the merge conflicts made it extremely difficult to merge. - created a new branch, and replicated his changes into it. Altered the tests slightly to use nameof()

---
+++ SqlServer.Rules.Test/TestScripts/Performance/AvoidNonsargableLikePattern/nonsargable_ignored.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[nonsargable_view_ignored] AS
+	-- GLOBAL IGNORE SRP0002
+	SELECT table1_id FROM [dbo].[table1] WHERE [data] LIKE '%what am I doing?'
+	UNION ALL
+	SELECT table1_id FROM [dbo].[table1] WHERE [data] LIKE '%I love lamp?'
+
+GO
---
+++ SqlServer.Rules.Test/TestScripts/Performance/AvoidNotEqualToRule/ansi_not_equal.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [dbo].[ansi_not_equal] AS
+	SELECT table1_id FROM [dbo].[table1] WHERE [data] <> 'what am I doing?'
+
+GO
